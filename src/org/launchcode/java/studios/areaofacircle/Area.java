3e22d81ca9151f344557878b59e40145b4e0bf6f
Andrew <drmadness1234@gmail.com>
2023-03-27 19:47:11 -0500

    done

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,23 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+public class Area {
+
+        public static void main(String[] args) {
+            double Radius;
+            double Area;
+            Scanner input;
+
+            input = new Scanner(System.in);
+            System.out.println("Enter a radius: ");
+            Radius = input.nextDouble();
+            input.close();
+
+            Area = Circle.getArea(Radius);
+            System.out.println("The area of a circle of radius " + Radius + " is: " + Area );
+        }
+
+
+}
+
+
